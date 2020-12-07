a32e67aa61742ca1086ea444e45b1e7a4cb7611a
baxtr <andrew@baxtrweb.com>
2020-12-07 00:09:45 -0500

    day 1 progress

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,17 @@
+/*let country = 'us';
+let continent = 'north america';
+let population = '300+ million';
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+/*
+let country = 'us';
+let isIsland = false;
+let population = 300000;
+let langauge = 'english';
+console.log(country);
+console.log(isIsland);
+console.log(population);
+console.log(langauge);
+*/
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+/*
+let js = 'bob';
+console.log(40 + 8 + 23 - 10);
+// Variable Name Conventions
+let firstName = 'bob';
+let lastName = 'bob';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+*/
+
+let javescriptIsFun = true;
+console.log(javescriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javescriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'jonas');
+
+javescriptIsFun = 'YES!';
+console.log(typeof javescriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2020;
+console.log(typeof year);
+
+console.log(typeof null);
